95740fa2c0d2cd81f2bbfe4b8361f0d535f9e402
xFruts <maxowinc@gmail.com>
2026-06-19 11:48:44 +0300

    initial commit

---
+++ practiceSpring/src/test/java/ru/maxow/practicespring/PracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maxow.practicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
